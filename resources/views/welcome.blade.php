5a4880f0a5d52018905cd487ac84cab76a00f7ab
rhino-it <it.academy.osh@gmail.com>
2021-01-04 22:42:54 +0600

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-{{ menu('Main', 'main.my_menu') }}
+{{--{{ menu('Main', 'main.my_menu') }}--}}
 {{--<!DOCTYPE html>--}}
 {{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
 {{--    <head>--}}
